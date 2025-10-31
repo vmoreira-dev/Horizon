8de1399a5160c7cf432893a3f32515039cf525ca
Vinicius <93095246+vmoreira-dev@users.noreply.github.com>
2025-10-31 14:58:57 -0400

    added more ui files

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500">
+      <h1 className="text-6xl font-bold text-white">Peak Horizon</h1>
+    </div>
+  );
+}
